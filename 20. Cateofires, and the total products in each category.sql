856b4d892268a774c67824338271fee766025a1f
sandeep16b <sandeep16b@gmail.com>
2018-08-05 18:28:20 -0400

    Added 20 & 21

---
+++ 20. Cateofires, and the total products in each category.sql
@@ -0,0 +1,3 @@
+--In the Customers table, show the total number of customers per Country and City.
+
+(select Country,City, Count(*) as TotalProducts from Northwind.Customers group by Country, City) order by TotalProducts desc
---
+++ 21. Total customers per country:city.sql
@@ -0,0 +1,7 @@
+--In the Customers table, show the total number of customers per Country and City.
+
+(select Country,City, Count(*) as TotalProducts from Northwind.Customers group by Country, City) order by TotalProducts desc
+
+--or
+
+(select Country,City, Count(*) as TotalProducts from Northwind.Customers group by City, Country) order by TotalProducts desc
